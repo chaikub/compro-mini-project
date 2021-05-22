3d5c316976c114aaac844c353ff59df779b2634d
chaikub <73686686+chaikub@users.noreply.github.com>
2021-05-22 12:47:40 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ struct data{
     char contact_number[20];
 };
 
-struct data it;
+struct data dat;
 
 void SetColor(int ForgC)
 {
@@ -60,43 +60,43 @@ void Setting_Color_And_Background(int ForgC, int BackC)
 }
 
 COORD coordinates = {0,0};
-void x_and_y_coordinates(int x, int y){
+void x_y_coordinates(int x, int y){
     coordinates.X = x; coordinates.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
 }
 
 void Frame(){
-    int a, b;
-    x_and_y_coordinates(0,0);
+    int a;
+    x_y_coordinates(0,0);
     printf("_");
     for(a = 1; a < 78; a++){
-        x_and_y_coordinates(a, 0);
+        x_y_coordinates(a, 0);
         printf("_");
     }
-    x_and_y_coordinates(78,0);
+    x_y_coordinates(78,0);
     printf("_");
     for(a = 1; a < 25; a++){
-        x_and_y_coordinates(78, a);
+        x_y_coordinates(78, a);
         if(a == 6){
             printf("|");
         }else{
             printf("|");
         }
     }
-    x_and_y_coordinates(78, 25);
+    x_y_coordinates(78, 25);
     printf("|");
     for(a = 77; a > 0; a--){
-        x_and_y_coordinates(a,25);
+        x_y_coordinates(a,25);
         if(a == 35){
             printf("_");
         }else{
             printf("_");
         }
     }
-    x_and_y_coordinates(0,25);
+    x_y_coordinates(0,25);
     printf("|");
     for(a = 24; a > 0; a--){
-        x_and_y_coordinates(0,a);
+        x_y_coordinates(0,a);
         if(a == 6){
             printf("|");
         }else{
@@ -105,7 +105,7 @@ void Frame(){
     }
 
     for(a = 1; a < 78; a++){
-        x_and_y_coordinates(a,6);
+        x_y_coordinates(a,6);
         if(a == 35){
             printf("_");
         }else{
@@ -114,7 +114,7 @@ void Frame(){
     }
 
     for(a = 7; a < 25; a++){
-        x_and_y_coordinates(35,a);
+        x_y_coordinates(35,a);
         printf("|");
     }
 
@@ -124,7 +124,7 @@ void Reset_Window(){
     int a,b;
     for(a = 37; a < 78; a++){
         for(b = 7; b < 25; b++){
-            x_and_y_coordinates(a,b);printf(" ");
+            x_y_coordinates(a,b);printf(" ");
         }
     }
     return;
@@ -132,7 +132,7 @@ void Reset_Window(){
 
 void window(){
     Frame();
-    x_and_y_coordinates(33,3);
+    x_y_coordinates(33,3);
     SetColor(35);
     printf("RECORD SYSTEM");
 }
@@ -140,12 +140,12 @@ void window(){
 
 void print_heading(const char st[]){
     Setting_Color_And_Background(31,28);
-    x_and_y_coordinates(38,8);printf("RECORD SYSTEM : %s",st);
+    x_y_coordinates(38,8);printf("RECORD SYSTEM : %s",st);
     Setting_Color_And_Background(17,15);
 }
 
 
-void adding_IT(){
+void adding_dat(){
     Reset_Window();
     print_heading("Add New");
     int print = 37;
@@ -156,81 +156,81 @@ void adding_IT(){
         MessageBox(0,"Error","Warning",0);
     }else{
         fflush(stdin);
-        x_and_y_coordinates(print,10);printf("ID: ");gets(it.id);
-        x_and_y_coordinates(print,12);printf("Name: ");gets(it.name);
-        x_and_y_coordinates(print,14);printf("Faculty: ");gets(it.faculty);
-        x_and_y_coordinates(print,16);printf("Department: ");gets(it.department);
-        x_and_y_coordinates(print,18);printf("Email: ");gets(it.email);
-        x_and_y_coordinates(print,20);printf("Contact Number: ");gets(it.contact_number);
-        fwrite(&it, sizeof(it), 1, openfile);
+        x_y_coordinates(print,10);printf("ID: ");gets(dat.id);
+        x_y_coordinates(print,12);printf("Name: ");gets(dat.name);
+        x_y_coordinates(print,14);printf("Faculty: ");gets(dat.faculty);
+        x_y_coordinates(print,16);printf("Department: ");gets(dat.department);
+        x_y_coordinates(print,18);printf("Email: ");gets(dat.email);
+        x_y_coordinates(print,20);printf("Contact Number: ");gets(dat.contact_number);
+        fwrite(&dat, sizeof(dat), 1, openfile);
 //       fwrite("\n", sizeof("\n"), 1, openfile);
-        x_and_y_coordinates(40,22); printf("New record is Added Successfully");
+        x_y_coordinates(40,22); printf("New record is Added Successfully");
     }
     SetColor(35);
     fclose(openfile);
     return;
 }
 
-void search_IT(){
+void search_dat(){
     Reset_Window();
     print_heading("Search Record");
     SetColor(45);
     char id[15];
     int isFound = 0;
-    x_and_y_coordinates(37,10);printf("Enter ID to Search: ");fflush(stdin);
+    x_y_coordinates(37,10);printf("Enter ID to Search: ");fflush(stdin);
     gets(id);
     FILE *openfile;
     openfile = fopen("record.txt","rb");
-    while(fread(&it,sizeof(it),1,openfile) == 1){
-        if(strcmp(id, it.id) == 0){
+    while(fread(&dat,sizeof(dat),1,openfile) == 1){
+        if(strcmp(id, dat.id) == 0){
             isFound = 1;
             break;
         }
     }
     if(isFound == 1){
-        x_and_y_coordinates(37,12);printf("The record is Found");
-        x_and_y_coordinates(37,14);printf("ID: %s",it.id);
-        x_and_y_coordinates(37,15);printf("Name: %s",it.name);
-        x_and_y_coordinates(37,16);printf("Faculty: %s",it.faculty);
-        x_and_y_coordinates(37,17);printf("Department: %s",it.department);
-        x_and_y_coordinates(37,18);printf("Email: %s",it.email);
-        x_and_y_coordinates(37,19);printf("Contact Number: %s",it.contact_number);
+        x_y_coordinates(37,12);printf("The record is Found");
+        x_y_coordinates(37,14);printf("ID: %s",dat.id);
+        x_y_coordinates(37,15);printf("Name: %s",dat.name);
+        x_y_coordinates(37,16);printf("Faculty: %s",dat.faculty);
+        x_y_coordinates(37,17);printf("Department: %s",dat.department);
+        x_y_coordinates(37,18);printf("Email: %s",dat.email);
+        x_y_coordinates(37,19);printf("Contact Number: %s",dat.contact_number);
     }else{
-        x_and_y_coordinates(37,12);printf("No record found in the database");
+        x_y_coordinates(37,12);printf("No record found in the database");
     }
     SetColor(35);
     fclose(openfile);
     return;
 }
 
-void modify_IT(){
+void modify_dat(){
     Reset_Window();
     print_heading("Modify Record");
     SetColor(45);
     char id[15];
     int isFound = 0, print = 37;
-    x_and_y_coordinates(37,10);printf("Enter ID to Modify: ");fflush(stdin);
+    x_y_coordinates(37,10);printf("Enter ID to Modify: ");fflush(stdin);
     gets(id);
     FILE *openfile;
     openfile = fopen("record.txt","rb+");
-    while(fread(&it, sizeof(it),1,openfile) == 1){
-        if(strcmp(id, it.id) == 0){
+    while(fread(&dat, sizeof(dat),1,openfile) == 1){
+        if(strcmp(id, dat.id) == 0){
             fflush(stdin);
-            x_and_y_coordinates(print,12);printf("ID: ");gets(it.id);
-            x_and_y_coordinates(print,13);printf("Name: ");gets(it.name);
-            x_and_y_coordinates(print,14);printf("Faculty: ");gets(it.faculty);
-            x_and_y_coordinates(print,15);printf("Department: ");gets(it.department);
-            x_and_y_coordinates(print,16);printf("Email: ");gets(it.email);
-            x_and_y_coordinates(print,17);printf("Contact Number: ");gets(it.contact_number);
-            fseek(openfile,-sizeof(it), SEEK_CUR);
-            fwrite(&it,sizeof(it), 1, openfile);
-            x_and_y_coordinates(40,22); printf("Record is Updated Successfully");
+            x_y_coordinates(print,12);printf("ID: ");gets(dat.id);
+            x_y_coordinates(print,13);printf("Name: ");gets(dat.name);
+            x_y_coordinates(print,14);printf("Faculty: ");gets(dat.faculty);
+            x_y_coordinates(print,15);printf("Department: ");gets(dat.department);
+            x_y_coordinates(print,16);printf("Email: ");gets(dat.email);
+            x_y_coordinates(print,17);printf("Contact Number: ");gets(dat.contact_number);
+            fseek(openfile,-sizeof(dat), SEEK_CUR);
+            fwrite(&dat,sizeof(dat), 1, openfile);
+            x_y_coordinates(40,22); printf("Record is Updated Successfully");
             isFound = 1;
             break;
         }
     }
     if(!isFound){
-        x_and_y_coordinates(print, 12);printf("No Record Found");
+        x_y_coordinates(print, 12);printf("No Record Found");
     }
     fclose(openfile);
     SetColor(35);
@@ -238,12 +238,10 @@ void modify_IT(){
 }
 
 
-void delete_IT(){
+void delete_dat(){
     Reset_Window();
     print_heading("Delete Record");
     SetColor(45);
-    char id[15];
-    int isFound = 0, print = 37;
     FILE *openfile, *temporary;
     openfile = fopen("record.txt","rb");
     temporary = fopen("temp.txt", "wb");
@@ -251,7 +249,7 @@ void delete_IT(){
     fclose(temporary);
     remove("record.txt");
     rename("temp.txt","record.txt");
-    x_and_y_coordinates(37,12);printf("All data has been successfully deleted.");
+    x_y_coordinates(37,12);printf("All data has been successfully deleted.");
     SetColor(35);
     return;
 }
@@ -261,25 +259,25 @@ void main_window(){
     SetColor(35);
     int x = 2;
     while(1){
-        x_and_y_coordinates(x,8);printf("1. Add");
-        x_and_y_coordinates(x,10);printf("2. Search");
-        x_and_y_coordinates(x,12);printf("3. Modify Record");
-        x_and_y_coordinates(x,14);printf("4. Delete Record");
-        x_and_y_coordinates(x,16);printf("5. Exit");
-        x_and_y_coordinates(x,18);printf("Enter your Choice: ");
+        x_y_coordinates(x,8);printf("1. Add");
+        x_y_coordinates(x,10);printf("2. Search");
+        x_y_coordinates(x,12);printf("3. Modify Record");
+        x_y_coordinates(x,14);printf("4. Delete Record");
+        x_y_coordinates(x,16);printf("5. Exit");
+        x_y_coordinates(x,18);printf("Enter your Choice: ");
         scanf("%d",&option);
         switch(option){
             case 1:
-                adding_IT();
+                adding_dat();
                 break;
             case 2:
-                search_IT();
+                search_dat();
                 break;
             case 3:
-                modify_IT();
+                modify_dat();
                 break;
             case 4:
-                delete_IT();
+                delete_dat();
                 break;
             case 5:
                 exit(0);
